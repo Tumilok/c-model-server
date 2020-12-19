7e8c6820ea1f2fbad9fa7b2051d1ac45a7bab56e
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-20 00:51:15 +0100

    Delete RoleReposiotry

--- src/main/java/pl/edu/agh/racing/cmodel/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.racing.cmodel.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.edu.agh.racing.cmodel.model.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
