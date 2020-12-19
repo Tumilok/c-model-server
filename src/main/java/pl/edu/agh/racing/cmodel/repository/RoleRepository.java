6ab0b8445a8cdbfc002cdcdeb3148a1899dd5576
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-19 23:39:12 +0100

    Add RoleRepository

--- src/main/java/pl/edu/agh/racing/cmodel/repository/RoleRepository.java
+++ src/main/java/pl/edu/agh/racing/cmodel/repository/RoleRepository.java
@@ -1,4 +1,9 @@
 package pl.edu.agh.racing.cmodel.repository;
 
-public class RoleRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.edu.agh.racing.cmodel.model.Role;
+
+@Repository
+public interface RoleRepository extends CrudRepository<Role, Long> {
 }
