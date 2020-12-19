7b5915c02ab68afdf04db4b41c29074041c2f3b0
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-19 23:42:35 +0100

    Add Token model

---
+++ src/main/java/pl/edu/agh/racing/cmodel/model/Token.java
@@ -0,0 +1,23 @@
+package pl.edu.agh.racing.cmodel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String token;
+    private Instant createdDate;
+}
