c67791879b3400077b8e750d37f36034c9e12eb9
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-19 23:38:32 +0100

    Add User model

--- src/main/java/pl/edu/agh/racing/cmodel/model/User.java
+++ src/main/java/pl/edu/agh/racing/cmodel/model/User.java
@@ -1,4 +1,32 @@
 package pl.edu.agh.racing.cmodel.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotBlank(message = "Surname is required")
+    private String surname;
+    @Email
+    @NotBlank(message = "Email is required")
+    private String email;
+    @NotBlank(message = "Password is required")
+    private String password;
+    private boolean isActivated;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roleId", referencedColumnName = "id")
+    private Role role;
 }
