28e824f680acc1ea6e21f59386077c30f6d4b75d
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-19 23:49:21 +0100

    Add RefreshTokenRepository

--- src/main/java/pl/edu/agh/racing/cmodel/repository/RefreshTokenRepository.java
+++ src/main/java/pl/edu/agh/racing/cmodel/repository/RefreshTokenRepository.java
@@ -1,4 +1,8 @@
 package pl.edu.agh.racing.cmodel.repository;
 
-public interface RefreshTokenRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshTokenRepository, Long> {
 }
