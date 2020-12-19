aa55d0fa94b7ac31e772e22399f5652fb74102ed
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2020-12-19 18:20:49 +0100

    Initial commit

---
+++ src/main/java/pl/edu/agh/racing/cmodel/CModelApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.racing.cmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/agh/racing/cmodel/CModelApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.racing.cmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
