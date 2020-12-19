bca381b0b039761eb64793a971befccbef0a860b
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-19 23:56:49 +0100

    Change extended interface

--- src/main/java/pl/edu/agh/racing/cmodel/repository/RefreshTokenRepository.java
+++ src/main/java/pl/edu/agh/racing/cmodel/repository/RefreshTokenRepository.java
@@ -1,8 +1,9 @@
 package pl.edu.agh.racing.cmodel.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pl.edu.agh.racing.cmodel.model.RefreshToken;
 
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshTokenRepository, Long> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 }
