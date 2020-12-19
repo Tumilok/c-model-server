5e3fb0600de0e34e9c1fc235eae309583f04bb94
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-20 00:50:37 +0100

    Add Log model

---
+++ src/main/java/pl/edu/agh/racing/cmodel/model/Log.java
@@ -0,0 +1,31 @@
+package pl.edu.agh.racing.cmodel.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Operation operation;
+    private Instant createdDate;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "simulationId", referencedColumnName = "id")
+    private Simulation simulation;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private User user;
+}
