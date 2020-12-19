0dcc0d3293879691503debf1e67256164f5c2d0b
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-19 23:39:29 +0100

    Add UserRepository

--- src/main/java/pl/edu/agh/racing/cmodel/repository/UserRepository.java
+++ src/main/java/pl/edu/agh/racing/cmodel/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package pl.edu.agh.racing.cmodel.repository;
 
-public interface UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.edu.agh.racing.cmodel.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
 }
