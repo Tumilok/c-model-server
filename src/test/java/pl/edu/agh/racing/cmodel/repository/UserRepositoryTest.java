8092bdc1f95f953e8db56b85a899f3d3d5ce0de2
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-21 22:44:00 +0100

    Update an implementation of checkSaveUser method

--- src/test/java/pl/edu/agh/racing/cmodel/repository/UserRepositoryTest.java
+++ src/test/java/pl/edu/agh/racing/cmodel/repository/UserRepositoryTest.java
@@ -3,11 +3,13 @@ package pl.edu.agh.racing.cmodel.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.ActiveProfiles;
 import pl.edu.agh.racing.cmodel.model.Role;
 import pl.edu.agh.racing.cmodel.model.User;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -17,10 +19,23 @@ public class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-//    @DisplayName("Test should pass when comment successfully saved to the database")
-    public void shouldSaveUser() {
-        User user = new User(null, "John", "Peterson", "john.peterson@gmail.com", "12345678", false, Role.NEWBIE);
-        User savedUser = userRepository.save(user);
-        assertThat(savedUser).usingRecursiveComparison().ignoringFields("userId").isEqualTo(user);
+    public void checkSaveUser() {
+        // given
+        User user1 = new User(null, "John", "Peterson",
+                "john.peterson@gmail.com", "12345678", false, Role.NEWBIE);
+        User user2 = new User(null, "Garik", "Jackson",
+                "garik.jackson@gmail.com", "qwerty", true, Role.MODERATOR);
+        User redundantUser = new User(null, "Garik", "Jackson",
+                "garik.jackson@gmail.com", "qwerty", true, Role.MODERATOR);
+
+        // when
+        User savedUser1 = userRepository.save(user1);
+        User savedUser2 = userRepository.save(user2);
+
+        // then
+        assertThat(savedUser1).usingRecursiveComparison().isEqualTo(user1);
+        assertThat(savedUser2).usingRecursiveComparison().isEqualTo(user2);
+        assertThrows(DataIntegrityViolationException.class,
+                () -> userRepository.saveAndFlush(redundantUser));
     }
 }
