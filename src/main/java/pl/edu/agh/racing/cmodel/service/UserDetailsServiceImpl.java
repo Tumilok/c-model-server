4ebda7be1ddb2514077bb59ae0a41be008296e17
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-26 13:39:25 +0100

    Add and implement UserDetailsServiceImpl
    LoadUserByUsername method was implemented using email as username and only with one 'USER' role. Moreover getAuthorities method was implemented which provides us with a singleton list of given role.

---
+++ src/main/java/pl/edu/agh/racing/cmodel/service/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package pl.edu.agh.racing.cmodel.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.edu.agh.racing.cmodel.model.User;
+import pl.edu.agh.racing.cmodel.repository.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("No user found with email: " + email));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
+                user.isEnabled(), true, true,
+                true, getAuthorities("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+}
