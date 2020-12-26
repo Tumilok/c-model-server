d2e45d5e54b800a7b1537f61725301115a600471
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-26 13:33:52 +0100

    Add LoginRequest container
    .his class represents data of client login request.

---
+++ src/main/java/pl/edu/agh/racing/cmodel/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.racing.cmodel.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
