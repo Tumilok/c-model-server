37674bcf3eff86ed481ff2c2ee5d74de1b72d119
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-26 01:10:27 +0100

    Add AuthController class
    Implemented signup and sendMail controllers.

---
+++ src/main/java/pl/edu/agh/racing/cmodel/controller/AuthController.java
@@ -0,0 +1,29 @@
+package pl.edu.agh.racing.cmodel.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.agh.racing.cmodel.dto.RegisterRequest;
+import pl.edu.agh.racing.cmodel.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful",
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Has Activated Successfully", HttpStatus.OK);
+    }
+}
