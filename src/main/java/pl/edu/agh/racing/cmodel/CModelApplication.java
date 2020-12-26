27150ae321720ef015f8c5bf34f62f86d87ebac2
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-26 01:08:55 +0100

    Enable Async computation
    In order to send notification emails faster.

--- src/main/java/pl/edu/agh/racing/cmodel/CModelApplication.java
+++ src/main/java/pl/edu/agh/racing/cmodel/CModelApplication.java
@@ -2,8 +2,10 @@ package pl.edu.agh.racing.cmodel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CModelApplication {
 
     public static void main(String[] args) {
