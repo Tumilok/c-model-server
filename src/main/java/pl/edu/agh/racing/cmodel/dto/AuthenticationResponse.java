3f22da7d4e6d2c4f97e7926cf0ee93ce4184fb18
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-26 13:34:55 +0100

    Add AuthenticationResponse container
    This class represents data which server sends to the client after login.

---
+++ src/main/java/pl/edu/agh/racing/cmodel/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.racing.cmodel.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private String authenticationToken;
+    private String username;
+}
