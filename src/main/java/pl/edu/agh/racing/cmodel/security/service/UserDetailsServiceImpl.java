4b10b769dc79920005a60c104ead9f99eff028c0
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-27 03:57:28 +0100

    Edit UserDetailsServiceImpl class
    Introduced UserDetailsImpl class to the loadUserByUsername method and removed getAuthorities method.

--- src/main/java/pl/edu/agh/racing/cmodel/security/service/UserDetailsServiceImpl.java
+++ src/main/java/pl/edu/agh/racing/cmodel/security/service/UserDetailsServiceImpl.java
@@ -1,8 +1,6 @@
 package pl.edu.agh.racing.cmodel.security.service;
 
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,9 +9,6 @@ import pl.edu.agh.racing.cmodel.model.User;
 import pl.edu.agh.racing.cmodel.repository.UserRepository;
 
 import javax.transaction.Transactional;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -24,14 +19,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        Optional<User> userOptional = userRepository.findByEmail(email);
-        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("No user found with email: " + email));
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(),
-                user.isEnabled(), true, true,
-                true, getAuthorities("USER"));
-    }
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No user found with email: " + email));
 
-    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
-        return Collections.singletonList(new SimpleGrantedAuthority(role));
+        return UserDetailsImpl.build(user);
     }
 }
