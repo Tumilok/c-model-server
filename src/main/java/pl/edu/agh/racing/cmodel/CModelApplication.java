d4901c2bdd5b96b941a3d5c7398cd759342749c5
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2021-03-02 21:49:31 +0100

    Adjust the swagger changes

--- src/main/java/pl/edu/agh/racing/cmodel/CModelApplication.java
+++ src/main/java/pl/edu/agh/racing/cmodel/CModelApplication.java
@@ -2,10 +2,13 @@ package pl.edu.agh.racing.cmodel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import pl.edu.agh.racing.cmodel.swagger.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class CModelApplication {
 
     public static void main(String[] args) {
